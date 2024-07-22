728b755610fa4cfcb848c7b5e2dd77c8fb52f630
s1tf <119539022+s1tf@users.noreply.github.com>
2024-07-22 22:01:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import math
 import arcade
 
 # Movement constants
-ROTATIONSPEED = 0.02
-MOVESPEED = 0.03
+ROTATION_SPEED = 0.02
+MOVE_SPEED = 0.03
 
-# Trigeometric tuples + variables for index
-TGM = (math.cos(ROTATIONSPEED), math.sin(ROTATIONSPEED))
-ITGM = (math.cos(-ROTATIONSPEED), math.sin(-ROTATIONSPEED))
+# Trigonometric tuples + variables for index
+TGM = (math.cos(ROTATION_SPEED), math.sin(ROTATION_SPEED))
+ITGM = (math.cos(-ROTATION_SPEED), math.sin(-ROTATION_SPEED))
 COS, SIN = (0, 1)
 
 # A map over the world
@@ -36,7 +36,8 @@ WORLD_MAP = [
 
 class App(arcade.Window):
     def __init__(self):
-        super().__init__()
+        super().__init__(fullscreen=True, center_window=True)
+        self.set_mouse_visible(False)
 
         # Defines starting position and direction
         self.position_x = 3.0
@@ -53,33 +54,40 @@ class App(arcade.Window):
         self.move_right = False
 
         # Define lines to draw
-        self.lines = []
+        self.line_width = 2
+        self.lines = [None] * self.width
+        self.border_lines = []
+        self.cell_id = None
 
     def on_draw(self):
         self.clear()
-        for line in self.lines:
-            arcade.draw_line(start_x=line[0], start_y=line[1], end_x=line[2], end_y=line[3], color=line[4], line_width=2)
+
+        arcade.draw_rectangle_filled(center_x=self.width/2, center_y=self.height/4, width=self.width, height=self.height/2, color=arcade.color.EERIE_BLACK)
+
+        if self.lines[0]:
+            arcade.draw_lines(point_list=self.lines, color=arcade.color.ARSENIC, line_width=self.line_width)
+            arcade.draw_points(self.lines, color=arcade.color.CHARLESTON_GREEN, size=self.line_width)
+            arcade.draw_lines(point_list=self.border_lines, color=arcade.color.CHARLESTON_GREEN, line_width=self.line_width)
 
     def on_update(self, delta_time):
-
         if self.move_up:
-            if not WORLD_MAP[int(self.position_x + self.direction_x * MOVESPEED)][int(self.position_y)]:
-                self.position_x += self.direction_x * MOVESPEED
-            if not WORLD_MAP[int(self.position_x)][int(self.position_y + self.direction_y * MOVESPEED)]:
-                self.position_y += self.direction_y * MOVESPEED
-        elif self.move_down:
-            if not WORLD_MAP[int(self.position_x - self.direction_x * MOVESPEED)][int(self.position_y)]:
-                self.position_x -= self.direction_x * MOVESPEED
-            if not WORLD_MAP[int(self.position_x)][int(self.position_y - self.direction_y * MOVESPEED)]:
-                self.position_y -= self.direction_y * MOVESPEED
-        elif self.move_left:
+            if not WORLD_MAP[int(self.position_x + self.direction_x * MOVE_SPEED)][int(self.position_y)]:
+                self.position_x += self.direction_x * MOVE_SPEED
+            if not WORLD_MAP[int(self.position_x)][int(self.position_y + self.direction_y * MOVE_SPEED)]:
+                self.position_y += self.direction_y * MOVE_SPEED
+        if self.move_down:
+            if not WORLD_MAP[int(self.position_x - self.direction_x * MOVE_SPEED)][int(self.position_y)]:
+                self.position_x -= self.direction_x * MOVE_SPEED
+            if not WORLD_MAP[int(self.position_x)][int(self.position_y - self.direction_y * MOVE_SPEED)]:
+                self.position_y -= self.direction_y * MOVE_SPEED
+        if self.move_left:
             old_direction_x = self.direction_x
             self.direction_x = self.direction_x * ITGM[COS] - self.direction_y * ITGM[SIN]
             self.direction_y = old_direction_x * ITGM[SIN] + self.direction_y * ITGM[COS]
             old_plane_x = self.plane_x
             self.plane_x = self.plane_x * ITGM[COS] - self.plane_y * ITGM[SIN]
             self.plane_y = old_plane_x * ITGM[SIN] + self.plane_y * ITGM[COS]
-        elif self.move_right:
+        if self.move_right:
             old_direction_x = self.direction_x
             self.direction_x = self.direction_x * TGM[COS] - self.direction_y * TGM[SIN]
             self.direction_y = old_direction_x * TGM[SIN] + self.direction_y * TGM[COS]
@@ -87,12 +95,12 @@ class App(arcade.Window):
             self.plane_x = self.plane_x * TGM[COS] - self.plane_y * TGM[SIN]
             self. plane_y = old_plane_x * TGM[SIN] + self.plane_y * TGM[COS]
 
-        if not self.lines or (self.move_up or self.move_down or self.move_left or self.move_right):
+        if not self.lines[0] or (self.move_up or self.move_down or self.move_left or self.move_right):
             # Starts drawing level from 0 to < self.width
-            column = 0
-            self.lines = []
-            while column < self.width:
-                camera_x = 2.0 * column / self.width - 1.0
+            self.border_lines = []
+            x = 0
+            for i in range(0, self.width, self.line_width):
+                camera_x = 2.0 * x / self.width - 1.0
                 ray_position_x = self.position_x
                 ray_position_y = self.position_y
                 ray_direction_x = self.direction_x + self.plane_x * camera_x
@@ -103,9 +111,11 @@ class App(arcade.Window):
                 map_y = int(ray_position_y)
 
                 # Delta distance calculation
-                # Delta = square ( raydir * raydir) / (raydir * raydir)
-                delta_distance_x = math.sqrt(1.0 + (ray_direction_y * ray_direction_y) / (ray_direction_x * ray_direction_x))
-                delta_distance_y = math.sqrt(1.0 + (ray_direction_x * ray_direction_x) / (ray_direction_y * ray_direction_y))
+                # Cache the re-used values
+                ray_direction_x_2 = ray_direction_x * ray_direction_x
+                ray_direction_y_2 = ray_direction_y * ray_direction_y
+                delta_distance_x = math.sqrt(1.0 + ray_direction_y_2 / ray_direction_x_2)
+                delta_distance_y = math.sqrt(1.0 + ray_direction_x_2 / ray_direction_y_2)
 
                 # We need side_distance_x and Y for distance calculation. Checks quadrant
                 if ray_direction_x < 0:
@@ -149,46 +159,53 @@ class App(arcade.Window):
 
                 # Calculating HEIGHT of the line to draw
                 line_height = abs(int(self.height / (perp_wall_distance + .0000001)))
-                draw_start = -line_height / 2.0 + self.height / 2.0
+                y_start = -line_height / 2.0 + self.height / 2.0
 
                 # if drawStat < 0 it would draw outside the screen
-                if draw_start < 0:
-                    draw_start = 0
+                if y_start < 0:
+                    y_start = 0
 
-                draw_end = line_height / 2.0 + self.height / 2.0
+                y_end = line_height / 2.0 + self.height / 2.0
 
-                if draw_end >= self.height:
-                    draw_end = self.height - 1
-
-                # Wall colors 0 to 3
-                wall_colors = [[], [150, 0, 0], [0, 150, 0], [0, 0, 150]]
-                color = wall_colors[WORLD_MAP[map_x][map_y]]
+                if y_end >= self.height:
+                    y_end = self.height - 1
 
                 # Drawing the graphics
-                self.lines.append([column, draw_start, column, draw_end, color])
-                # arcade.draw_line(start_x=column, start_y=draw_start, end_x=column, end_y=draw_end, color=color, line_width=2)
-                column += 2
+                self.lines[i] = [x, y_start]
+                self.lines[i+1] = [x, y_end]
+                # print(f'x: {x}, y_start: {y_start}, y_end: {y_end}')
+
+                cell_id = WORLD_MAP[map_x][map_y]
+                if cell_id != self.cell_id:
+                    self.border_lines.append([self.lines[i][0], self.lines[i][1]])
+                    self.border_lines.append([self.lines[i + 1][0], self.lines[i + 1][1]])
+                self.cell_id = cell_id
+
+                x += self.line_width
 
     def on_key_press(self, key, key_modifiers):
 
+        if key == arcade.key.ESCAPE:
+            arcade.exit()
+
         if key in [arcade.key.UP, arcade.key.W]:
             self.move_up = True
-        elif key in [arcade.key.DOWN, arcade.key.S]:
+        if key in [arcade.key.DOWN, arcade.key.S]:
             self.move_down = True
-        elif key in [arcade.key.LEFT, arcade.key.A]:
+        if key in [arcade.key.LEFT, arcade.key.A]:
             self.move_left = True
-        elif key in [arcade.key.RIGHT, arcade.key.D]:
+        if key in [arcade.key.RIGHT, arcade.key.D]:
             self.move_right = True
 
     def on_key_release(self, key, key_modifiers):
 
         if key in [arcade.key.UP, arcade.key.W]:
             self.move_up = False
-        elif key in [arcade.key.DOWN, arcade.key.S]:
+        if key in [arcade.key.DOWN, arcade.key.S]:
             self.move_down = False
-        elif key in [arcade.key.LEFT, arcade.key.A]:
+        if key in [arcade.key.LEFT, arcade.key.A]:
             self.move_left = False
-        elif key in [arcade.key.RIGHT, arcade.key.D]:
+        if key in [arcade.key.RIGHT, arcade.key.D]:
             self.move_right = False
 
 
